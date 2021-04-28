a65b837b97bd2361c0599e3a9082983e3b36d615
Muhammad Afif Zainuddin <binzainudm@coventry.ac.uk>
2021-04-28 18:00:07 +0000

    test

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const Koa = require('koa');
-const app = new Koa();
-const cors = require('@koa/cors');
-
-app.use(cors());
-
-const special = require('./routes/special.js')
-const listings = require('./routes/listings.js');
-const users = require('./routes/users.js');
-
-app.use(special.routes());
-app.use(listings.routes());
-app.use(users.routes());
-
+const app = require('./app');
 let port = process.env.PORT || 3000;
-
 app.listen(port);
 console.log(`API server running on port ${port}`)
